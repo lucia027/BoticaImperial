7f577479202cb85893b337355ba46111f97d2c95
lucia027 <fuertes.lucia.27@gmail.com>
2026-08-09 12:33:25 +0200

    Dtos terminados.

---
+++ BoticaImperial/BoticaImperial/Dto/CasoMedicoDto.cs
@@ -0,0 +1,19 @@
+﻿using System.Xml.Serialization;
+
+namespace BoticaImperial.Dto;
+
+[XmlRoot("BoticaImperial")]
+[XmlType("Caso Medico")]
+public record CasoMedicoDto(
+    [property: XmlAttribute("")] int Id,
+    [property: XmlAttribute("tituloCaso")] string TituloCaso,
+    [property: XmlAttribute("sintomas")] string Sintomas,
+    [property: XmlAttribute("fechaInicio")] string FechaInicio,
+    [property: XmlAttribute("gravedad")] string Gravedad,
+    [property: XmlAttribute("causaSospecha")] string CausaSospecha,
+    [property: XmlAttribute("sustanciasSospechosas")] HashSet<SustanciaDto>? SustanciasSospechosas,
+    [property: XmlAttribute("tratamientosAplicados")] HashSet<SustanciaDto>? TratamientosAplicados,
+    [property: XmlAttribute("estado")] string Estado
+) {
+    public CasoMedicoDto() : this(0, "", "", "", "", "", null, null, "") { }
+}
---
+++ BoticaImperial/BoticaImperial/Dto/SustanciaDto.cs
@@ -0,0 +1,39 @@
+﻿
+using System.Xml.Serialization;
+
+namespace BoticaImperial.Dto;
+
+[XmlRoot("BoticaImperial")]
+[XmlType("Sustancia")]
+public record SustanciaDto(
+    [property: XmlAttribute("id")] int Id,
+    [property: XmlAttribute("nombre")] string Nombre,
+    [property: XmlAttribute("descripcion")] string Descripcion,
+    [property: XmlAttribute("precio")] string Precio,
+    [property: XmlAttribute("disponibilidad")] string Disponibilidad,
+    [property: XmlAttribute("nivelPeligro")] string NivelPeligro,
+    [property: XmlAttribute("tipo")] string Tipo,
+    
+    [property: XmlAttribute("sintomas")] string? Sintomas,
+    [property: XmlAttribute("dosisRecomendada")] string? DosisRecomendada,
+    [property: XmlAttribute("efectosSecundarios")] string? EfectosSecundarios,
+    [property: XmlAttribute("tiempoDeEfecto")] int? TiempoDeEfecto,
+    
+    [property: XmlAttribute("intensidadDelEfecto")] int? IntensidadDelEfecto,
+    [property: XmlAttribute("duracion")] int? Duracion,
+    [property: XmlAttribute("contraIndicaciones")] string? ContraIndicaciones,
+    [property: XmlAttribute("riegoUsoExcesivo")] string? RiegoUsoExcesivo,
+    
+    [property: XmlAttribute("viaDeAdministracion")] string? ViaDeAdministracion,
+    [property: XmlAttribute("tiempoAparicionSintomas")] int? TiempoAparicionSintomas,
+    [property: XmlAttribute("antidoto")] SustanciaDto? Antidoto,
+    [property: XmlAttribute("gradoDeToxicidad")] int? GradoDeToxicidad,
+    [property: XmlAttribute("probabilidadSupervivencia")] int? ProbabilidadSupervivencia,
+    
+    [property: XmlAttribute("createAt")] string CreateAt,
+    [property: XmlAttribute("updateAt")] string UpdateAt,
+    [property: XmlAttribute("deleteAt")] string DeleteAt,
+    [property: XmlAttribute("isDelete")] bool IsDelete
+) {
+    public SustanciaDto() : this(0, "", "", "", "", "", "", "", "", "", 0, 0, 0, "", "", "", 0, null, 0, 0, "", "", "", false) { }
+}
